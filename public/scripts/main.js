261b6df3d2578d788f9c5c256984dd1654a6a823
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-12-17 10:07:12 -0500

    added typescript context

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,133 +1,62 @@
-const get = (selector) => {
-  return window.document.querySelector(selector);
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
-
-const getAll = (selector) => {
-  return window.document.querySelectorAll(selector);
+var get = function (selector) {
+    return window.document.querySelector(selector);
 };
-
-const grabContext = (selector) =>
-  window.document.querySelector(selector).getContext("2d");
-
-
-const delay = (callback, timeout) => setTimeout(callback, timeout);
-
-const loop = (handler, timeout) => setInterval(handler, timeout);
-
-const endLoop = (handler) => clearInterval(handler);
-
-const endDelay = (handler) => clearTimeout(handler);
-
-const getUrl = () => {
-  return window.document.URL;
+var getAll = function (selector) {
+    return window.document.querySelectorAll(selector);
 };
-
-const listen = (elementSelector, type, callback) =>
-  get(elementSelector).addEventListener(type, callback);
-
-const numberSort = (array) => array.sort((a, b) => a - b);
-
-const filterOut = (array, valueToFilterOut) =>
-  array.filter((element) => element !== valueToFilterOut);
-
+var delay = function (callback, timeout) { return setTimeout(callback, timeout); };
+var loop = function (handler, timeout) { return setInterval(handler, timeout); };
+var endLoop = function (handler) { return clearInterval(handler); };
+var endDelay = function (handler) { return clearTimeout(handler); };
+var getUrl = function () {
+    return window.document.URL;
+};
+var listen = function (elementSelector, type, callback) { var _a; return (_a = get(elementSelector)) === null || _a === void 0 ? void 0 : _a.addEventListener(type, callback); };
+var numberSort = function (array) { return array.sort(function (a, b) { return a - b; }); };
+var filterOut = function (array, valueToFilterOut) {
+    return array.filter(function (element) { return element !== valueToFilterOut; });
+};
 Object.defineProperty(Array.prototype, "toWords", {
-  value: function () {
-    return [].concat(this).join("");
-  },
+    value: function () {
+        return [].concat(this).join("");
+    }
 });
-
 Object.defineProperty(Array.prototype, "add", {
-  value: function (value) {
-    return [...this, value];
-  },
+    value: function (value) {
+        return __spreadArray(__spreadArray([], this, true), [value], false);
+    }
 });
-
 Object.defineProperty(Array.prototype, "log", {
-  value: function () {
-    console.log(this);
-  },
+    value: function () {
+        console.log(this);
+    }
 });
-
 Object.defineProperty(Number.prototype, "log", {
-  value: function () {
-    console.log(this);
-  },
+    value: function () {
+        console.log(this);
+    }
 });
-
 Object.defineProperty(String.prototype, "log", {
-  value: function () {
-    console.log(this);
-  },
+    value: function () {
+        console.log(this);
+    }
 });
-
 Object.defineProperty(String.prototype, "reverse", {
-  value: function () {
-    return this.split("").reverse().join("");
-  },
+    value: function () {
+        return this.split("").reverse().join("");
+    }
 });
-
 Object.defineProperty(Number.prototype, "reverse", {
-  value: function () {
-    return this.split("").reverse().join("");
-  }
-})
-
-const twoSum = (nums, target) => {
-  let map = new Map();
-
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (map.get(num) === undefined) map.set(target - num, i);
-    else return [map.get(num), i];
-  }
-};
-
-const romanToInt = (s) => {
-  const legend = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
-  let total = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    if (legend[s[i]] < legend[s[i + 1]]) {
-      total += legend[s[i + 1]] - legend[s[i]];
-      i++;
-    } else {
-      total += legend[s[i]];
+    value: function () {
+        return this.split("").reverse().join("");
     }
-  }
-
-  return total;
-};
-
-const intToRoman = (num) => {
-  let obj = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-  };
-  let str = "";
-  for (let key in obj) {
-    while (num >= obj[key]) {
-      str += key;
-      num -= obj[key];
-    }
-  }
-  return str;
-};
+});
---
+++ public/scripts/main.ts
@@ -0,0 +1,66 @@
+const get = (selector: keyof HTMLElement) => {
+    window.document.querySelector(selector);
+};
+
+const getAll = (selector: keyof HTMLElement) => {
+    return window.document.querySelectorAll(selector);
+};
+
+const delay = (callback: () => void, timeout: number) => setTimeout(callback, timeout);
+
+const loop = (handler: () => void, timeout: number) => setInterval(handler, timeout);
+
+const endLoop = (handler: number) => clearInterval(handler);
+
+const endDelay = (handler: number) => clearTimeout(handler);
+
+
+const listen = (elementSelector: keyof HTMLElement, type: keyof ElementEventMap, callback: () => void) =>
+    get(elementSelector)?.addEventListener(type, callback);
+
+const numberSort = (array: Array<number>) => array.sort((a, b) => a - b);
+
+const filterOut = (array: Array<String | number>, valueToFilterOut: String | number) =>
+    array.filter((element) => element !== valueToFilterOut);
+
+Object.defineProperty(Array.prototype, "toWords", {
+    value: function () {
+        return [].concat(this).join("");
+    },
+});
+
+Object.defineProperty(Array.prototype, "add", {
+    value: function (value: String | number) {
+        return [...this, value];
+    },
+});
+
+Object.defineProperty(Array.prototype, "log", {
+    value: function () {
+        console.log(this);
+    },
+});
+
+Object.defineProperty(Number.prototype, "log", {
+    value: function () {
+        console.log(this);
+    },
+});
+
+Object.defineProperty(String.prototype, "log", {
+    value: function () {
+        console.log(this);
+    },
+});
+
+Object.defineProperty(String.prototype, "reverse", {
+    value: function () {
+        return this.split("").reverse().join("");
+    },
+});
+
+Object.defineProperty(Number.prototype, "reverse", {
+    value: function () {
+        return this.split("").reverse().join("");
+    }
+})

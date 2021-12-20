84996dbc74475dfb323292e892357ec4161aeb45
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-12-20 09:18:35 -0500

    added comments

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,4 @@
+"use strict";
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
@@ -7,21 +8,77 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
+exports.__esModule = true;
+exports.filterOut = exports.numberSort = exports.listen = exports.endDelay = exports.endLoop = exports.loop = exports.delay = exports.getAll = exports.get = void 0;
+/**
+ *
+ * @param selector ID or class of the element you want to return
+ * @returns HTMLElement of given selector
+ */
 var get = function (selector) {
     return window.document.querySelector(selector);
 };
+exports.get = get;
+/**
+ *
+ * @param selector ID or class of the element you want to return
+ * @returns All HTMLElements of given selector
+ */
 var getAll = function (selector) {
     return window.document.querySelectorAll(selector);
 };
+exports.getAll = getAll;
+/**
+ *
+ * @param callback The function you want to run after the delay
+ * @param timeout The amount of time for the delay
+ */
 var delay = function (callback, timeout) { return setTimeout(callback, timeout); };
+exports.delay = delay;
+/**
+ *
+ * @param handler The function you want to run after the delay
+ * @param timeout The amount of between loops
+ */
 var loop = function (handler, timeout) { return setInterval(handler, timeout); };
+exports.loop = loop;
+/**
+ *
+ * @param handler The number of the loop or the variable that is used to start the loop
+ */
 var endLoop = function (handler) { return clearInterval(handler); };
+exports.endLoop = endLoop;
+/**
+ *
+ * @param handler The number of the delay or the variable that is used to start the delay
+ */
 var endDelay = function (handler) { return clearTimeout(handler); };
-var listen = function (elementSelector, type, callback) { var _a; return (_a = get(elementSelector)) === null || _a === void 0 ? void 0 : _a.addEventListener(type, callback); };
-var numberSort = function (array) { return array.sort(function (a, b) { return a - b; }); };
+exports.endDelay = endDelay;
+/**
+ *
+ * @param elementSelector ID or class of the element you want to add the listener to.
+ * @param type Type of event to listen for ex: 'click'
+ * @param callback The function you want to run when the event is triggered
+ */
+var listen = function (elementSelector, type, callback) { var _a; return (_a = (0, exports.get)(elementSelector)) === null || _a === void 0 ? void 0 : _a.addEventListener(type, callback); };
+exports.listen = listen;
+/**
+ *
+ * @param array The array you want to sory
+ * @returns The array sorted by number value
+ */
+var numberSort = function (array) { return array.sort(function (a, b) { return +a - +b; }); };
+exports.numberSort = numberSort;
+/**
+ *
+ * @param array The array you want to filter
+ * @param valueToFilterOut The value to filter out
+ * @returns The array without the filtered value
+ */
 var filterOut = function (array, valueToFilterOut) {
     return array.filter(function (element) { return element !== valueToFilterOut; });
 };
+exports.filterOut = filterOut;
 Object.defineProperty(Array.prototype, "toWords", {
     value: function () {
         return [].concat(this).join("");
--- public/scripts/main.ts
+++ public/scripts/main.ts
@@ -1,26 +1,64 @@
-const get = (selector: keyof HTMLElement) => {
+/**
+ * 
+ * @param selector ID or class of the element you want to return
+ * @returns HTMLElement of given selector
+ */
+export const get = (selector: keyof HTMLElement) => {
     return window.document.querySelector(selector);
 };
-
-const getAll = (selector: keyof HTMLElement) => {
+/**
+ * 
+ * @param selector ID or class of the element you want to return
+ * @returns All HTMLElements of given selector
+ */
+ export const getAll = (selector: keyof HTMLElement) => {
     return window.document.querySelectorAll(selector);
 };
+/**
+ * 
+ * @param callback The function you want to run after the delay
+ * @param timeout The amount of time for the delay
+ */
+ export const delay = (callback: () => void, timeout: number) => setTimeout(callback, timeout);
+/**
+ * 
+ * @param handler The function you want to run after the delay
+ * @param timeout The amount of between loops
+ */
+ export const loop = (handler: () => void, timeout: number) => setInterval(handler, timeout);
+/**
+ * 
+ * @param handler The number of the loop or the variable that is used to start the loop
+ */
+ export const endLoop = (handler: number) => clearInterval(handler);
+/**
+ * 
+ * @param handler The number of the delay or the variable that is used to start the delay
+ */
+ export const endDelay = (handler: number) => clearTimeout(handler);
 
-const delay = (callback: () => void, timeout: number) => setTimeout(callback, timeout);
-
-const loop = (handler: () => void, timeout: number) => setInterval(handler, timeout);
-
-const endLoop = (handler: number) => clearInterval(handler);
-
-const endDelay = (handler: number) => clearTimeout(handler);
-
-
-const listen = (elementSelector: keyof HTMLElement, type: keyof ElementEventMap, callback: () => void) =>
+/**
+ * 
+ * @param elementSelector ID or class of the element you want to add the listener to.
+ * @param type Type of event to listen for ex: 'click'
+ * @param callback The function you want to run when the event is triggered
+ */
+ export const listen = (elementSelector: keyof HTMLElement, type: keyof ElementEventMap, callback: () => void) =>
     get(elementSelector)?.addEventListener(type, callback);
 
-const numberSort = (array: Array<number>) => array.sort((a, b) => a - b);
-
-const filterOut = (array: Array<String | number>, valueToFilterOut: String | number) =>
+/**
+ * 
+ * @param array The array you want to sory
+ * @returns The array sorted by number value
+ */
+ export const numberSort = (array: Array<number | string>) => array.sort((a, b) => +a - +b);
+/**
+ * 
+ * @param array The array you want to filter
+ * @param valueToFilterOut The value to filter out
+ * @returns The array without the filtered value
+ */
+ export const filterOut = (array: Array<String | number>, valueToFilterOut: String | number) =>
     array.filter((element) => element !== valueToFilterOut);
 
 Object.defineProperty(Array.prototype, "toWords", {
